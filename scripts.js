499ea2fe6417f7ad1c17f85b84515a027f960ecb
w0Lart <olegkostuk2009@gmail.com>
2023-10-16 09:31:05 +0300

    add functions to create 16 and 64 grids
    and add functions to paint squares into a gray, white and rainbow colors

--- scripts.js
+++ scripts.js
@@ -3,14 +3,93 @@ const container = document.createElement("div");
 container.className = "container";
 body.appendChild(container);
 
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.className = "grid";
-  container.appendChild(grid);
-  grid.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = 'gray';
-  },
-  false,
-  );
-}
+
+
+
+function createGrid16(){
+  let deletion = container.querySelectorAll(".grid64");
+  deletion.forEach(grid=>{
+    grid.remove();
+  });
+  let deletion1 = container.querySelectorAll(".grid16");
+  deletion1.forEach(grid=>{
+    grid.remove();
+  });
+  for (let i = 0; i < 256; i++) {
+    let grid = document.createElement("div");
+    grid.className = "grid16";
+    container.appendChild(grid);
+  }
+};
+createGrid16();
+
+function createGrid64(){
+  let deletion = container.querySelectorAll(".grid16");
+  deletion.forEach(grid=>{
+    grid.remove();
+  });
+  let deletion1 = container.querySelectorAll(".grid64");
+  deletion1.forEach(grid=>{
+    grid.remove();
+  });
+  for (let i=0; i<4096; i++){
+    let grid = document.createElement("div");
+    grid.className = "grid64";
+    container.appendChild(grid);
+  }
+};
+
+function grayColorGrid(){
+  let grid16 = document.querySelectorAll(".grid16");
+  grid16.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'gray';
+    });
+  });
+  let grid64 = document.querySelectorAll(".grid64");
+  grid64.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'gray';
+    });
+  });
+};
+
+function removerColorGrid(){
+  let grid16 = document.querySelectorAll(".grid16");
+  grid16.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'white';
+    });
+  });
+  let grid64 = document.querySelectorAll(".grid64");
+  grid64.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'white';
+    });
+  });
+};
+
+function rainbowColorGrid(){
+  function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  }; 
+  let grid16 = document.querySelectorAll(".grid16");
+  grid16.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = randomColor();
+    });
+  });
+  let grid64 = document.querySelectorAll(".grid64");
+  grid64.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = randomColor();
+    });
+  });
+};
+
+
 
